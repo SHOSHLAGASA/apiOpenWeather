c35537ad7b585105a4400650ef47aca2cbbda648
shoshana lagasa <shoshanalagasa850@gmail.com>
2022-03-08 22:30:48 -0600

    update

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 let apiKey = "cf002751564a4c78f5f7ed479f1b9ba3";
-// getting the button form the page
- let button = document.querySelector("#Search");
 //  getting the date to display on the page
  let name = document.querySelector("#name")
  let desc = document.querySelector("#desc")
  let tem = document.querySelector("#temp")
 //  when the button listen to click and its empty the respond will be Please Enter A city if they click city the the API will get the date 
- button.addEventListener("click", () => {
+document
+    .querySelector("#Search")
+    .addEventListener("click", () => { 
     let cityName = document.querySelector("#city-Name").value;
     if (cityName === "") {
         alert("Please Enter A city");
     } else {
-        const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-        console.log(api);
-        fetch(api)
+        const getDataByCity = async (city) => {
+            const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+            return fetch(api)
+        }
+        getDataByCity(cityName)
         .then((response) => {
             return response.json();
          })
-         .then((data) => {
+         .then((data) => { 
+console.log(`data`, data)
              let nameValue = data["name"];
              let temp = data["main"]["temp"];
              let descValue = data["weather"][0]["description"];
@@ -26,10 +29,13 @@ let apiKey = "cf002751564a4c78f5f7ed479f1b9ba3";
              console.log(temp);
              console.log(descValue);
 
-             name.innerHTML = `Name: ${nameValue}`;
-				tem.innerHTML = `Temp: ${temp}`;
-				desc.innerHTML = `Description: ${descValue}`;
+             setResponseHtml(nameValue, temp, descValue)
 
+             const setResponseHtml = (nameValue, temp, descValue) => {
+                name.innerHTML = `Name: ${nameValue}`;
+                tem.innerHTML = `Temp: ${temp}`;
+                desc.innerHTML = `Description: ${descValue}`;
+            }
          })
          .catch((error) => {
              console.log("Error: ", error);
@@ -38,3 +44,4 @@ let apiKey = "cf002751564a4c78f5f7ed479f1b9ba3";
     }
 
 });
+
